6212cac941d8b0face49d342d61be72d8ce0f9e4
Pascal van Kooten <kootenpv@gmail.com>
2016-10-30 14:04:32 +0100

    expanduser+expandpath, more complete coveralls

--- just/__init__.py
+++ just/__init__.py
@@ -6,6 +6,7 @@ just.run
 just.print_version
 """
 
+import os
 import just.txt as txt
 import just.json_ as json
 import just.yaml_ as yaml
@@ -25,16 +26,21 @@ EXT_TO_MODULE = {
 }
 
 
+def join_path(*args):
+    return os.path.expanduser(os.path.join(*args))
+
+
 def _read(fname):
-    ext = fname.split(".")[-1] if "." in fname else "txt"
+    ext = fname.split(".")[-1] if "." in fname[-5:] else "txt"
     return EXT_TO_MODULE[ext].read(fname)
 
 
-def read(fname):
+def read(*fnargs):
+    fname = fnargs[-1]
     if isinstance(fname, list):
-        return [_read(fn) for fn in fname]
+        return [_read(join_path(*fnargs[:-1] + (fn,))) for fn in fname]
     else:
-        return _read(fname)
+        return _read(join_path(*fnargs))
 
 
 def _write(obj, fname):
@@ -43,11 +49,13 @@ def _write(obj, fname):
         return EXT_TO_MODULE[ext].write(obj, fname)
 
 
-def write(obj, fname):
+def write(obj, *fnargs):
+    fname = fnargs[-1]
     if isinstance(fname, list):
         if not isinstance(obj, list):
             raise NotImplementedError
         else:
-            return [_write(o, fn) for o, fn in zip(obj, fname)]
+            return [_write(o, join_path(*fnargs[:-1] + (fn,)))
+                    for o, fn in zip(obj, fname)]
     else:
-        return _write(obj, fname)
+        return _write(obj, join_path(*fnargs))
