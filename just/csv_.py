06e2e90d6123fcd2dd7bef1ac11349a7188ec652
Vincent Kotwizkiy <vincent@kotwizkiy.ru>
2016-11-01 09:22:07 +0500

    Adding optional argument for CSV delimiters

--- just/csv_.py
+++ just/csv_.py
@@ -1,9 +1,13 @@
 
-def read(fn):
+def read(fn, delim=None):
+    if delim is None:
+        delim = ","
     with open(fn) as f:
-        return [x.rstrip('\n').split(",") for x in f]
+        return [x.rstrip('\n').split(delim) for x in f]
 
 
-def write(obj, fn):
+def write(obj, fn, delim=None):
+    if delim is None:
+        delim = ","
     with open(fn, "w") as f:
-        f.write("\n".join([",".join(x) for x in obj]))
+        f.write("\n".join([delim.join(x) for x in obj]))
