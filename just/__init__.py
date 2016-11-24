ef170c8ddb95d7965f70a38581da291321f9602d
Pascal van Kooten <kootenpv@gmail.com>
2016-11-24 19:06:42 +0100

    added multi_read, in need of refactor

--- just/__init__.py
+++ just/__init__.py
@@ -7,6 +7,7 @@ just.print_version
 """
 
 import os
+import glob
 import just.txt as txt
 import just.json_ as json
 import just.yaml_ as yaml
@@ -15,7 +16,7 @@ import just.pickle_ as pickle
 from just.dir import mkdir
 
 __project__ = "just"
-__version__ = "0.0.9"
+__version__ = "0.1.14"
 
 EXT_TO_MODULE = {
     "html": txt,
@@ -40,6 +41,10 @@ def _read(fname, no_exist):
     return EXT_TO_MODULE[ext].read(fname)
 
 
+def multi_read(star_path, no_exist="Throw"):
+    return [_read(x, no_exist) for x in glob.glob(os.path.expanduser(star_path))]
+
+
 def read(*fnargs, **kwargs):
     no_exist = kwargs.get("no_exist", "Throw")
     fname = fnargs[-1]
@@ -49,7 +54,9 @@ def read(*fnargs, **kwargs):
         return _read(join_path(*fnargs), no_exist=no_exist)
 
 
-def _write(obj, fname, mkdir_no_exist):
+def _write(obj, fname, mkdir_no_exist, skip_if_exist):
+    if skip_if_exist and os.path.isfile(fname):
+        return False
     if mkdir_no_exist:
         dname = os.path.dirname(fname)
         if dname not in set([".", "..", ""]):
@@ -61,15 +68,22 @@ def _write(obj, fname, mkdir_no_exist):
 
 def write(obj, *fnargs, **kwargs):
     mkdir_no_exist = kwargs.get("mkdir_no_exist", True)
+    skip_if_exist = kwargs.get("skip_if_exist", False)
     fname = fnargs[-1]
     if isinstance(fname, list):
         if not isinstance(obj, list):
             raise NotImplementedError("Only list of fnames + list of objects supported.")
         else:
-            return [_write(o, join_path(*fnargs[:-1] + (fn,)), mkdir_no_exist=mkdir_no_exist)
+            return [_write(o,
+                           join_path(*fnargs[:-1] + (fn,)),
+                           mkdir_no_exist=mkdir_no_exist,
+                           skip_if_exist=skip_if_exist)
                     for o, fn in zip(obj, fname)]
     else:
-        return _write(obj, join_path(*fnargs), mkdir_no_exist=mkdir_no_exist)
+        return _write(obj,
+                      join_path(*fnargs),
+                      mkdir_no_exist=mkdir_no_exist,
+                      skip_if_exist=skip_if_exist)
 
 
 def iread(fname):
