7e6c7d3c8d4e5d92caf7b13bbc6ed52510c92aa6
Pascal van Kooten <kootenpv@gmail.com>
2017-03-07 14:16:20 +0100

    log symlink

--- just/log.py
+++ just/log.py
@@ -1,10 +1,15 @@
 import time
 import sys
 import just
+import os
 
 START = "_".join(time.asctime().replace(":", "_").split())
+NAME = sys.argv[0].rstrip(".py")
+LOG_BASE = "logs"
+LOG_FILE = "{}/{}_{}.jsonl".format(LOG_BASE, NAME, START)
+LOG_LINK = "{}/{}.jsonl".format(LOG_BASE, NAME)
 
-LOG_FILE = "logs/{}_{}.jsonl".format(sys.argv[0].rstrip(".py"), START)
+os.symlink(just.make_path(LOG_FILE), just.make_path(LOG_LINK))
 
 
 def log(obj, *tags):
