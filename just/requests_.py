f41bd9e70bfee9744e38e60cc36ed978b949df58
Pascal van Kooten <kootenpv@gmail.com>
2017-06-07 16:57:34 +0200

    added content (raw) capability

--- just/requests_.py
+++ just/requests_.py
@@ -2,7 +2,7 @@ import time
 import json
 
 
-def _retry(request_fn, max_retries, delay_base, kwargs):
+def _retry(request_fn, max_retries, delay_base, raw, kwargs):
     from requests import RequestException
     tries = 0
     if 'headers' not in kwargs:
@@ -19,23 +19,25 @@ def _retry(request_fn, max_retries, delay_base, kwargs):
             if tries == max_retries:
                 return ""
             time.sleep(delay_base ** tries)
+    if raw:
+        return r.content
     if "application/json" in r.headers['Content-Type']:
         return r.json()
     return r.text
 
 
-def get(url, params=None, max_retries=5, delay_base=3, **kwargs):
+def get(url, params=None, max_retries=5, delay_base=3, raw=False, **kwargs):
     import requests
     kwargs['url'] = url
     kwargs['params'] = json.dumps(params) if params else ''
-    result = _retry(requests.get, max_retries, delay_base, kwargs)
+    result = _retry(requests.get, max_retries, delay_base, raw, kwargs)
     return result
 
 
-def post(url, params=None, data=None, max_retries=5, delay_base=3, **kwargs):
+def post(url, params=None, data=None, max_retries=5, raw=False, delay_base=3, **kwargs):
     import requests
     kwargs['url'] = url
     kwargs['params'] = json.dumps(params) if params else ''
     kwargs['data'] = json.dumps(data) if data else ''
-    result = _retry(requests.post, max_retries, delay_base, kwargs)
+    result = _retry(requests.post, max_retries, delay_base, raw, kwargs)
     return result
