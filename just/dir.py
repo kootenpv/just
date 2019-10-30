8f2cd75b258bba1619b2684a1e8f19160800f291
Pascal van Kooten <pascal@vks.ai>
2019-10-30 13:22:39 +0100

    file permission mode to mkdir

--- just/dir.py
+++ just/dir.py
@@ -2,9 +2,9 @@ import os
 import errno
 
 
-def mkdir(path):
+def mkdir(path, mode=0o777):
     try:
-        os.makedirs(path)
+        os.makedirs(path, mode)
     # Python >2.5
     except OSError as exc:  # pragma: no cover
         if exc.errno == errno.EEXIST and os.path.isdir(path):
