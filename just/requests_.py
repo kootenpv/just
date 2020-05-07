4e4696f011d24be47a9e5c88eb5c4b3fad0612bf
pascal <pascal@vks.ai>
2020-05-08 01:41:27 +0200

    finalized caching to use domain, adding sleep_time option

--- just/requests_.py
+++ just/requests_.py
@@ -1,6 +1,4 @@
-import os
 import time
-import json
 import requests
 import diskcache
 from just.dir import mkdir
@@ -8,48 +6,86 @@ from just.dir import mkdir
 session = None
 
 caches = {}
+timers = {}
+sessions = {}
 
 
-def _retry(request_fn, max_retries, delay_base, raw, kwargs):
+def _retry(method, max_retries, delay_base, raw, cache_key, sleep_time, kwargs):
     from requests import RequestException
 
     tries = 0
-    # if 'headers' not in kwargs:
-    #     kwargs["headers"] = {}
-    # if 'User-Agent' not in kwargs:
-    #     kwargs["headers"]['User-Agent'] = 'Just Agent 1.0'
+    url = kwargs["url"]
+    domain_name = url.split("/")[2].split("?")[0].replace("www.", "")
+
+    if cache_key:
+
+        if domain_name not in caches:
+            base = mkdir("~/.just_requests/")
+            caches[domain_name] = diskcache.Cache(base + domain_name)
+
+        if cache_key in caches[domain_name]:
+            return caches[domain_name][cache_key]
+
     if "timeout" not in kwargs:
         kwargs["timeout"] = delay_base
+
+    if domain_name not in sessions:
+        sessions[domain_name] = requests.Session()
+
+    # e.g. GET or POST
+    request_fn = getattr(sessions[domain_name], method)
+
+    if sleep_time and domain_name in timers:
+        # 1200 - 1201 + 3
+        diff = timers[domain_name] - time.time() + sleep_time
+
+        if diff > 0:
+            time.sleep(diff)
+
+    # retrying
     while tries < max_retries:
         try:
             r = request_fn(**kwargs)
             if r.status_code > 399:
-                return None
+                r = None
             break
         except RequestException as e:
             tries += 1
             print("just.requests_", kwargs["url"], "attempt", tries, str(e))
             if tries == max_retries:
-                return ""
+                r = ""
+                break
             time.sleep(delay_base ** tries)
-    if raw:
-        return r.content
-    if "application/json" in r.headers['Content-Type']:
-        return r.json()
-    return r.text
+
+    timers[domain_name] = time.time()
+
+    # result handling
+    if r is None or r == "":
+        pass
+    elif raw:
+        r = r.content
+    elif "application/json" in r.headers['Content-Type']:
+        r = r.json()
+    else:
+        r = r.text
+
+    if cache_key:
+        caches[domain_name][cache_key] = r
+
+    return r
 
 
-def get(url, params=None, max_retries=1, delay_base=3, raw=False, use_cache=False, **kwargs):
-    cache_key = (url, params)
-    if use_cache:
-        cache_name = url.split("/")[2].split("?")[0]
-
-        if cache_name not in caches:
-            base = mkdir("~/.just_requests/")
-            caches[cache_name] = diskcache.Cache(base + cache_name)
-
-        if use_cache and cache_key in caches[cache_name]:
-            return caches[cache_name][cache_key]
+def get(
+    url,
+    params=None,
+    max_retries=1,
+    delay_base=3,
+    raw=False,
+    use_cache=False,
+    sleep_time=None,
+    **kwargs
+):
+    cache_key = (url, params) if use_cache else False
 
     global session
     if session is None:
@@ -58,10 +94,8 @@ def get(url, params=None, max_retries=1, delay_base=3, raw=False, use_cache=Fals
     kwargs['url'] = url
     if params is not None:
         kwargs['params'] = params
-    result = _retry(session.get, max_retries, delay_base, raw, kwargs)
 
-    if use_cache:
-        caches[cache_name][cache_key] = result
+    result = _retry("get", max_retries, delay_base, raw, cache_key, sleep_time, kwargs)
 
     return result
 
@@ -75,22 +109,10 @@ def post(
     json=None,
     delay_base=3,
     use_cache=False,
+    sleep_time=None,
     **kwargs
 ):
-    cache_key = (url, params, data, json)
-    if use_cache:
-        cache_name = url.split("/")[2].split("?")[0]
-
-        if cache_name not in caches:
-            base = mkdir("~/.just_requests/")
-            caches[cache_name] = diskcache.Cache(base + cache_name)
-
-        if use_cache and cache_key in caches[cache_name]:
-            return caches[cache_name][cache_key]
-
-    global session
-    if session is None:
-        session = requests.Session()
+    cache_key = (url, params, data, json) if use_cache else False
 
     kwargs['url'] = url
     if params is not None:
@@ -100,9 +122,6 @@ def post(
     if json is not None:
         kwargs["json"] = json
 
-    result = _retry(session.post, max_retries, delay_base, raw, kwargs)
-
-    if use_cache:
-        caches[cache_name][cache_key] = result
+    result = _retry("post", max_retries, delay_base, raw, cache_key, sleep_time, kwargs)
 
     return result
