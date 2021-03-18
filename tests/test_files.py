60af6cad20fdff46748b6572a2561b9af80ea3f7
pascal <pascal@vks.ai>
2021-03-18 22:15:25 +0100

    fix test

--- tests/test_files.py
+++ tests/test_files.py
@@ -45,13 +45,9 @@ def test_compare(m, extension, inp, expected, compare):
 def test_multi_read():
     obj = ["a", "b"]
     fnames = ["a.txt", "b.txt"]
-    just.multi_write(obj, fnames)
     try:
-        full_names = just.glob("*.txt")
-        multi_content = just.multi_read("*.txt")
-        for o, f in zip(obj, fnames):
-            full_name = [x for x in full_names if x.endswith(f)][0]
-            assert multi_content[full_name] == o
+        for name, data in just.multi_read("*.txt"):
+            assert fnames.index(name) == obj.index(data)
     finally:
         for fname in fnames:
             os.remove(fname)
